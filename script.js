5eb603b93fc9fabe176956f03cbbe65961faae96
Peter <peters1989@mail.ru>
2022-08-04 18:41:56 +0500

    Js function reverse

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let arr = [1, 2, 3, 4]
+let mas = ['name', 'pit']
+function rev(x) {
+
+    let a = []
+    a = x.reverse()
+    return a 
+}
+console.log(rev(arr));
+
+
+
+
+console.log(rev(mas));
